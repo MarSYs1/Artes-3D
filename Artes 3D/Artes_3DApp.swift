77c466e2922a17bece42d01aafab1e391e2328bf
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-03-14 07:47:09 +0100

    Initial Commit

---
+++ Artes 3D/Artes_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Artes_3DApp.swift
+//  Artes 3D
+//
+//  Created by Mauro Sys One on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Artes_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
